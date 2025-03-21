c325280a8e1b86855e1f6995d1e5a63337a0eb43
Rajgosavi7878 <135588760+Rajgosavi7878@users.noreply.github.com>
2025-03-21 17:48:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/save", async (req,res)=>{
-	const url = "mongodb+srv://gosaviraj66:HquMuGOUxp1Uj2ra@cluster0.wp0gj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-	const client = new MongoClient(url);
-	const db = client.db("ss_19march25");
-	const coll = db.collection("student");
-	const doc = {"name":req.body.name,"company":req.body.company,"pkg":req.body.pkg};
-	console.log(doc);
-	const result = await coll.insertOne(doc)
-	.then(result => res.send(result))
-	.catch(error => res.send(error));
+// Check DB Connected Or Not
+const url =
+  "mongodb+srv://gosaviraj66:cy5BaJ2RKEbc0rQm@cluster0.j5pus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Connect to your Atlas cluster
+
+
+ /*
+async function run() {
+  try {
+    await client.connect();
+    console.log("Successfully connected to Atlas");
+  } catch (err) {
+    console.log(err.stack);
+  } 
+} 
+
+run().catch(console.dir);
+*/
+
+app.post("/save", (req, res) => {
+  const client = new MongoClient(url);
+  const db = client.db("fb_19march25");
+  const coll = db.collection("student");
+  const doc = { name: req.body.name, feedback: req.body.feedback };
+  console.log(doc);
+  coll
+    .insertOne(doc)
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error));
 });
 
-app.listen(11000,()=>{console.log("ready to serve @ 11000");});
-
-
-// HquMuGOUxp1Uj2ra
+app.listen(11000, () => {
+  console.log("ready @ 11000");
+});
